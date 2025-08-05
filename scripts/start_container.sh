66e24053bb0e0484b319544073a4f31448dc6617
Akash05061 <asuttakoti@gmail.com>
2025-08-05 16:00:08 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull akash501/simple-python-flask-app:latest
-
+ 
 # Run the Docker image as a container
 docker run -d -p 8100:5000 akash501/simple-python-flask-app:latest
 
