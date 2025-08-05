72212e132967d9a8094119ea80a9cb811dc83ea8
Akash05061 <asuttakoti@gmail.com>
2025-08-05 12:16:14 +0530

    Update start-container.sh

--- srcipts/start-container.sh
+++ srcipts/start-container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull akash501/sample-python-app-flask:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 akash501/sample-python-app-flask:latest
+
